ae815a09ea1db1019c7823f210cc7ea3802de442
c69219 <ashley.g.williams@hud.gov>
2025-10-13 13:14:19 -0400

    Cleaned up pricing descriptions

--- src/app/_core/models/membership.models.ts
+++ src/app/_core/models/membership.models.ts
@@ -18,21 +18,21 @@ export const MembershipPlans: MembershipInfo[] = [{
         name: 'Free',
         monthlyPrice: 0,
         description: 'Play Chatter Up! for free',
-        features: ['Unlimited practice sessions', 'Numeric message scoring', 'Access to all environments', 'View greatest hits', 'No account required'],
+        features: ['Unlimited practice sessions', 'Numeric message scoring', 'Access to all environments', 'View greatest hits', 'No credit card required'],
         popular: false,
     }, {
         id: MembershipType.Paid,
         name: 'Paid',
         monthlyPrice: 4.99,
-        description: 'Full access with detailed feedback',
-        features: ['Everything in Guest',  'Detailed score explanations', 'Personal progress tracking', 'Conversation history', 'Weekly tips newsletter'],
+        description: 'Detailed coach feedback',
+        features: ['Everything in Free',  'Detailed score explanations', 'Personal progress tracking', 'Conversation history', 'Weekly tips newsletter'],
         popular: true,
     }, {
         id: MembershipType.Premium,
         name: 'Premium',
         monthlyPrice: 7.49,
-        description: 'Full access with detailed feedback and coach suggestions',
-        features: ['Everything in Basic Member', 'Coach suggestions upon request', 'Conversation Rewinds', 'Invitations to special events', 'Priority support'],
+        description: 'Access to all features',
+        features: ['Everything in Paid Member', 'Coach suggestions upon request', 'Conversation Rewinds', 'Invitations to special events', 'Priority support'],
         popular: false,
     }
 ];
