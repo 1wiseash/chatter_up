1574984046fa61dec12bc5308522275e1d73dd3f
Ashley Williams <my.yellow.dog@gmail.com>
2025-12-29 13:25:10 -0500

    Fixed About page sign-up bug

--- src/app/about-page/about-page.ts
+++ src/app/about-page/about-page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@services';
 
 @Component({
   selector: 'cu-about-page',
@@ -8,5 +9,7 @@ import { RouterLink } from '@angular/router';
   styleUrl: './about-page.css'
 })
 export class AboutPage {
+  readonly _authService = inject(AuthService);
+  readonly loggedIn = this._authService.loggedIn;
 
 }
